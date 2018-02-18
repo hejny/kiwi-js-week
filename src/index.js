d5b7035ef1472abed66f63a41eb78eec62a3b0c1
hejny <me@pavolhejny.com>
2018-02-18 02:02:30 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -1,79 +1,38 @@
 import React from 'react';
-import { intercept,observe, observable,spy,autorunAsync} from 'mobx';
+import {observe, observable} from 'mobx';
 import ReactDOM from 'react-dom';
-import URI from 'urijs';
-import moment from 'moment';
+import {uriToState, stateToUri} from './routing';
 import App from './components/App';
 import DataModel from './DataModel';
 
-const dataModel = new DataModel();
-pushUriToState();
+const rootElement = document.getElementById('root');
 
+function restartApp(/*event=null*/) {
 
-function pushUriToState(state) {
+    const dataModel = new DataModel();
     dataModel.search = observable(uriToState(window.document.location));
+    /*if(event){
+        console.log(event.state);
+        dataModel.flights = observable(event.state);
+    }*/
 
     if (dataModel.search.from.id !== '' && dataModel.search.to.id !== '') {
         dataModel.searchFlights();
     }
 
 
-    observe(dataModel.search, observer );
-    observe(dataModel.search.pagination, observer );
-    observe(dataModel.search.pagination.sort, observer );
+    function observer() {
+        window.history.pushState(/*dataModel.flights*/{}, window.document.title, stateToUri(dataModel.search));
+    }
 
+    observe(dataModel.search, observer);
+    observe(dataModel.search.pagination, observer);
+    observe(dataModel.search.pagination.sort, observer);
 
+    window.onpopstate = restartApp;
 
-
-    window.onpopstate = pushUriToState;
-
-
-    ReactDOM.render(<App dataModel={dataModel}/>, document.getElementById('root'));
+    //rootElement.innerHTML = '';
+    ReactDOM.render(<App dataModel={dataModel}/>, rootElement);
 }
 
-
-
-function stateToUri(state){
-    return URI('/')
-        .query({
-            from: state.from.id,
-            to: state.to.id,
-            date: moment(state.date).format('YYYY-MM-DD'),
-            page: state.pagination.page,
-            sortBy: state.pagination.sort.by,
-            sortAsc: state.pagination.sort.asc,
-        })
-        .toString();
-}
-function uriToState(uri){
-    const query = URI(uri).search(true);
-    console.log(query);
-    return({
-        from: {
-            name: query.from||'Prague',
-            id: query.from||'prague_cz',
-        },
-        to: {
-            name: query.to||'Barcelona',
-            id: query.to||'barcelona_es',
-        },
-        date: moment(query.date||'2018-03-16').toDate(),
-        pagination: {
-            page: parseInt(query.page||'0'),
-            itemsPerPage: 5,
-            sort: {
-                by: query.sortBy||'date',
-                asc: query.sortAsc||true=='true'
-            }
-        }
-    });
-}
-
-
-
-
-
-function observer(){
-    window.history.pushState({}, window.document.title, stateToUri(dataModel.search));
-}
-
+restartApp();
---
+++ src/routing.js
@@ -0,0 +1,39 @@
+import URI from "urijs";
+import moment from "moment";
+
+export function stateToUri(state) {
+    return URI('/')
+        .query({
+            from: state.from.id,
+            to: state.to.id,
+            date: moment(state.date).format('YYYY-MM-DD'),
+            page: state.pagination.page,
+            sortBy: state.pagination.sort.by,
+            sortAsc: state.pagination.sort.asc,
+        })
+        .toString();
+}
+
+export function uriToState(uri) {
+    const query = URI(uri).search(true);
+    console.log(query);
+    return ({
+        from: {
+            name: query.from || 'Prague',
+            id: query.from || 'prague_cz',
+        },
+        to: {
+            name: query.to || 'Barcelona',
+            id: query.to || 'barcelona_es',
+        },
+        date: moment(query.date || '2018-03-16').toDate(),
+        pagination: {
+            page: parseInt(query.page || '0'),
+            itemsPerPage: 5,
+            sort: {
+                by: query.sortBy || 'date',
+                asc: query.sortAsc || true == 'true'
+            }
+        }
+    });
+}
